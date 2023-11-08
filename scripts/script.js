10774df5fab0abcd1039e8ed71c8b8490a2d684a
marcanthonybakker <118165939+marcanthonybakker@users.noreply.github.com>
2023-11-08 10:34:52 +0100

    Versie 2.1
    Bijna klaar!!! Nog een laatste netheid check hierna

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@
 /* Aanroepen van buttons en locaties voor JS interactie */
 /********************************************************/
 
+//Checken of de JS gelinkt is
 console.log("Howdy!");
 
 // Navigatie menu
@@ -33,6 +34,15 @@ const imgNg = document.querySelector("#imgNg");
 const imgF91 = document.querySelector("#imgF91");
 const imgVic = document.querySelector("#imgVic");
 
+// Focus light op img
+const img78Focus = document.querySelector("#img78 img");
+const imgMkFocus = document.querySelector("#imgMk img");
+const imgZetaFocus = document.querySelector("#imgZeta img");
+const imgZzFocus = document.querySelector("#imgZz img");
+const imgNgFocus = document.querySelector("#imgNg img");
+const imgF91Focus = document.querySelector("#imgF91 img");
+const imgVicFocus = document.querySelector("#imgVic img");
+
 // Warning op computerscherm
 const computerScreen = document.querySelector("#computerScreen");
 
@@ -59,6 +69,8 @@ easterEggLiftOff.addEventListener('click', function() {
 // Zero state
 zeroStateOff.addEventListener('click', function() {
     zeroState.classList.add('menuHidden');
+    img78.classList.add('slideAnimatie');
+    img78Focus.classList.add('focusLight');
     playAudio('menu.mp3');
 });
 
@@ -89,8 +101,10 @@ menuButtonHide.addEventListener('click', function() {
 function checkScrollPosition(currentPosition) {
     // de case moet true zijn, anders gaat default af
     switch (true) {
+
     // Tussen positie eerste en tweede pixel locatie gaat de rest van de acties af
     case currentPosition >= 0 && currentPosition <= 158:
+
         // Alle text info per specifieke Mobile Suit dat veranderd wordt
         liJaar.textContent = 'Jaar: UC 0079';
         liNaam.textContent = 'Naam: RX 79-2 Gundam';
@@ -98,14 +112,23 @@ function checkScrollPosition(currentPosition) {
         liGewicht.textContent = 'Gewicht: 60.0 Ton';
         liPower.textContent = 'Energie: 1380 kW';
         liSnelheid.textContent = 'Snelheid: 0.93 G';
+
         // De faction afbeelding locatie wordt veranderd
         factionImg.src = './images/efsf.png';
+
         // De link locatie per specifieke Mobile Suit dat veranderd wordt
         link.href = 'https://gundam.fandom.com/wiki/RX-78-2_Gundam';
-        // als img78 etc er niet is, voeg dan de class toe, d.m.v. logical not operator
-        if (!img78.classList.contains('slideAnimatie')) {
-            img78.classList.add('slideAnimatie');
-        }
+
+        // de aria-label beschrijving veranderd per passende naam => meer informatie over (Suit naam 1 tot de rest)
+        menuButton.ariaLabel = 'Meer informatie over Mobile Suit: RX 79-2 Gundam';
+
+        // voeg de slide animatie toe als je van positie veranderd
+        img78.classList.add('slideAnimatie');
+
+        // voeg een focus light aan de mobile suit
+        img78Focus.classList.add('focusLight');
+
+        // verander de tekst van het extra informatie scherm
         extraInfo.textContent = 'De RX-78-2 Gundam is het kenmerkende mobiele pak van de televisieserie Mobile Suit Gundam. Als onderdeel van de RX-78 Gundam serie werd het in het geheim gebouwd op Side 7. De Gundam zou de loop van de oorlog ten gunste van de Aardse Federatie veranderen tijdens de Eenjarige Oorlog tegen het Vorstendom Zeon. Het werd voornamelijk bestuurd door Amuro Ray.';
         break;
     case currentPosition >= 520 && currentPosition <= 701:
@@ -117,9 +140,9 @@ function checkScrollPosition(currentPosition) {
         liSnelheid.textContent = 'Snelheid: 1.50 G';
         factionImg.src = './images/aeug.png';
         link.href = 'https://gundam.fandom.com/wiki/RX-178_Gundam_Mk-II';
-        if (!imgMk.classList.contains('slideAnimatie')) {
-            imgMk.classList.add('slideAnimatie');
-        }
+        menuButton.ariaLabel = 'Meer informatie over Mobile Suit: RX 79-2 RX-178 Gundam Mk-II';
+        imgMk.classList.add('slideAnimatie');
+        imgMkFocus.classList.add('focusLight');
         extraInfo.textContent = 'De RX-178 Gundam Mk-II is een mobiel pak dat geïntroduceerd wordt in de anime serie Mobile Suit Zeta Gundam. Het is de opvolger van de bekende RX-78-2 Gundam die diende tijdens de Eenjarige Oorlog.';
         break;
     case currentPosition >= 1110 && currentPosition <= 1291:
@@ -130,10 +153,10 @@ function checkScrollPosition(currentPosition) {
         liPower.textContent = 'Energie: 2020 kW';
         liSnelheid.textContent = 'Snelheid: 1.81 G';
         factionImg.src = './images/aeug.png';
+        menuButton.ariaLabel = 'Meer informatie over Mobile Suit: MSZ-006 Zeta Gundam';
         link.href = 'https://gundam.fandom.com/wiki/MSZ-006_Zeta_Gundam';
-        if (!imgZeta.classList.contains('slideAnimatie')) {
-            imgZeta.classList.add('slideAnimatie');
-        }
+        imgZeta.classList.add('slideAnimatie');
+        imgZetaFocus.classList.add('focusLight');
         extraInfo.textContent = 'De MSZ-006 Zeta Gundam was het kenmerkende mobiele pak van de anime Mobile Suit Zeta Gundam. Het verscheen ook in de vervolgserie Mobile Suit Gundam ZZ.';
         break;
     case currentPosition >= 1700 && currentPosition <= 1981:
@@ -144,10 +167,10 @@ function checkScrollPosition(currentPosition) {
         liPower.textContent = 'Energie: 7340 kW';
         liSnelheid.textContent = 'Snelheid: 1.48 G';
         factionImg.src = './images/aeug.png';
+        menuButton.ariaLabel = 'Meer informatie over Mobile Suit: MSZ-010 ZZ Gundam';
         link.href = 'https://gundam.fandom.com/wiki/MSZ-010_%CE%96%CE%96_Gundam';
-        if (!imgZz.classList.contains('slideAnimatie')) {
-            imgZz.classList.add('slideAnimatie');
-        }
+        imgZz.classList.add('slideAnimatie');
+        imgZzFocus.classList.add('focusLight');
         extraInfo.textContent = 'De MSZ-010 ZZ Gundam (ook bekend als ZZ Gundam, ZZ; uitgesproken als "Dubbele Zeta") is het kenmerkende mobiele pak van de anime Mobile Suit Gundam ZZ en wordt bestuurd door Judau Ashta.';
         break;
     case currentPosition >= 2290 && currentPosition <= 2468:
@@ -158,10 +181,10 @@ function checkScrollPosition(currentPosition) {
         liPower.textContent = 'Energie: 2980 kW';
         liSnelheid.textContent = 'Snelheid: 1.55 G';
         factionImg.src = './images/londoBell.png';
+        menuButton.ariaLabel = 'Meer informatie over Mobile Suit: RX-93 Nu Gundam';
         link.href = 'https://gundam.fandom.com/wiki/MSZ-006_Zeta_Gundam';
-        if (!imgNg.classList.contains('slideAnimatie')) {
-            imgNg.classList.add('slideAnimatie');
-        }
+        imgNg.classList.add('slideAnimatie');
+        imgNgFocus.classList.add('focusLight');
         extraInfo.textContent = 'De RX-93 Nu Gundam (ook bekend als Nu Gundam, Nu) is een mobiel pak geïntroduceerd in Mobile Suit Gundam: Chars Counterattack. Het werd ontworpen en bestuurd door Amuro Ray.';
         break;
     case currentPosition >= 2880 && currentPosition <= 3058:
@@ -172,10 +195,10 @@ function checkScrollPosition(currentPosition) {
         liPower.textContent = 'Energie: 4250 kW';
         liSnelheid.textContent = 'Snelheid: 4.44 G';
         factionImg.src = './images/efsf.png';
+        menuButton.ariaLabel = 'Meer informatie over Mobile Suit: F91 Gundam F91';
         link.href = 'https://gundam.fandom.com/wiki/RX-93_%CE%BD_Gundam';
-        if (!imgF91.classList.contains('slideAnimatie')) {
-            imgF91.classList.add('slideAnimatie');
-        }
+        imgF91.classList.add('slideAnimatie');
+        imgF91Focus.classList.add('focusLight');
         extraInfo.textContent = 'De F91 Gundam F91 (F91 ガンダムF91, Effu Kujūichi Gandamu Fōmyura Nainti Wan?) is een mobiel pak geïntroduceerd in de film Mobile Suit Gundam F91, waarin het werd bestuurd door Seabook Arno. Harrison Madin bestuurde twee productietype eenheden in aangepaste kleuren.';
         break;
     case currentPosition >= 3405 && currentPosition <= 3756:
@@ -186,14 +209,14 @@ function checkScrollPosition(currentPosition) {
         liPower.textContent = 'Energie: 4780 kW';
         liSnelheid.textContent = 'Snelheid: 4.50 G';
         factionImg.src = './images/leagueMilitaire.png';
+        menuButton.ariaLabel = 'Meer informatie over Mobile Suit: LM312V04 Victory Gundam';
         link.href = 'https://gundam.fandom.com/wiki/LM312V04_Victory_Gundam';
-        if (!imgVic.classList.contains('slideAnimatie')) {
-            imgVic.classList.add('slideAnimatie');
-        }
+        imgVic.classList.add('slideAnimatie');
+        imgVicFocus.classList.add('focusLight');
         extraInfo.textContent = 'De LM312V04 Victory Gundam (ook bekend als V1) is het kenmerkende mobiele pak van Mobile Suit Victory Gundam, meestal bestuurd door Üso Ewin totdat hij zijn opvolger, de LM314V21 Victory 2 Gundam, verkreeg.';
         break;
-      default:
-        // Haal de animatie weg op elke andere scrollocatie om de animatie herhalend opnieuw te plaatsen wanneer je weer over de juiste positie gaat.
+    default:
+        // Haal de animatie weg op elke andere scrollocatie om de animatie herhalend opnieuw te plaatsen wanneer je weer over de juiste positie gaat
         img78.classList.remove('slideAnimatie');
         imgMk.classList.remove('slideAnimatie');
         imgMk.classList.remove('easterEggAnimatie');
@@ -202,6 +225,15 @@ function checkScrollPosition(currentPosition) {
         imgNg.classList.remove('slideAnimatie');
         imgF91.classList.remove('slideAnimatie');
         imgVic.classList.remove('slideAnimatie');
+
+        // Verwijder het oud focuslicht als het Mobile Suit veranderd 
+        img78Focus.classList.remove('focusLight');
+        imgMkFocus.classList.remove('focusLight');
+        imgZetaFocus.classList.remove('focusLight');
+        imgZzFocus.classList.remove('focusLight');
+        imgNgFocus.classList.remove('focusLight');
+        imgF91Focus.classList.remove('focusLight');
+        imgVicFocus.classList.remove('focusLight');
     }
 }
 
